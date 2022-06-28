284fff7269b03e07f9793fa889b44aa89568afd2
FrogLancer <108316153+FrogLancer@users.noreply.github.com>
2022-06-27 21:31:19 -0600

    Add files via upload

---
+++ tip-calculator.py
@@ -0,0 +1,13 @@
+# Day 2, Tip Calculator
+
+
+print("Welcome to the tip calculator.")
+
+totalBill = float(input("What is the total bill?: "))
+numPeople = int(input("How many people to split the bill?: "))
+percentTip = float(input("What percentage would you like to tip?: "))
+
+billWithTip = totalBill + (totalBill * (percentTip / 100))
+eachToPay = round(billWithTip / numPeople, 2)
+
+print(f"Each person should pay: {eachToPay}")
