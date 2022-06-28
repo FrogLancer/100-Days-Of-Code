0fa11346c03fee74aedf137cc69295580b54f9d4
FrogLancer <108316153+FrogLancer@users.noreply.github.com>
2022-06-27 21:29:11 -0600

    Delete day_1.py

--- day_1.py
+++
@@ -1,9 +0,0 @@
-# Day 1, Band Name Generator
-
-
-print('Welcome to the Band Name Generator.')
-
-cityName = input("What's the name city you grew up in?: ")
-petName = input("What's your pet's name?: ")
-
-print(f"Your band name could be {cityName} {petName}.")
